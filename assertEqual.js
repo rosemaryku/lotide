649cffdd1eee2d3b4b03cb26ac375b56651c30e3
Rosemary Ku <rosemary.ku@hotmail.com>
2022-01-10 15:20:27 -0500

    Created assertEqual logic

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("12weekbootcamp", "12weekbootcamp");
+assertEqual("12", 12);
