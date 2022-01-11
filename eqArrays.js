5107704911dedacb26d84bc4491bd1d7e7e855dc
Rosemary Ku <rosemary.ku@hotmail.com>
2022-01-11 10:26:49 -0500

    Added eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,14 @@
 const eqArrays = function(actual, expected) {
 
-  
-  if (actual === expected) {
+  if (actual.length === expected.length && actual.every((val,index) => val === expected[index])){
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-// TEST CODE
+// TESTS: 
 eqArrays([1, 2, 3], [1, 2, 3]) // => true
-// eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
