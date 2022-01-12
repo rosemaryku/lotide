d4a40fd8b9c3836c1d84222f3a0e412a5b663d8e
Rosemary Ku <rosemary.ku@hotmail.com>
2022-01-12 11:36:20 -0500

    Add countOnly function

---
+++ countLetters.js
@@ -0,0 +1,39 @@
+// Cannot use regular assertEqual() since objects are reference types.
+
+const assertEqual = function(actual, expected) {
+  actual = JSON.stringify(actual); 
+  expected = JSON.stringify(expected);
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function countLetters(str) {
+  let revStr = str.split(" ").join(""); 
+  let result = {};
+  for (let letter of revStr){
+    if (result[letter]){
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  // console.log(result);
+  return result;
+
+};
+
+assertEqual(countLetters("lighthouse in the house"), {
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+});
--- countOnly.js
+++ countOnly.js
@@ -12,7 +12,7 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    if (itemsToCount[item]) { 
+    if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
       } else {
@@ -22,7 +22,7 @@ const countOnly = function(allItems, itemsToCount) {
   }
   console.log(results);
   return results;
-}
+};
 
 const firstNames = [
   "Karl",
