2f2c3b3cd367ace627f8979af35ef54b16cb645f
Rosemary Ku <rosemary.ku@hotmail.com>
2022-01-13 13:31:06 -0500

    Fix small syntax errors

--- findKey.js
+++ findKey.js
@@ -6,14 +6,14 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-function findKey(object, callback) {
+const findKey = function(object, callback) {
   let keyArray = Object.keys(object);
   for (let key of keyArray) {
     if (callback(object[key]) === true) {
       return key;
     }
   }
-}
+};
 
 // TESTS:
 console.log(findKey({
@@ -23,14 +23,14 @@ console.log(findKey({
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)) // => "noma"
+}, x => x.stars === 2)); // => "noma"
 
 
 assertEqual(findKey({
-    "Blue Hill": { stars: 1 },
-    "Akaleri":   { stars: 3 },
-    "noma":      { stars: 2 },
-    "elBulli":   { stars: 3 },
-    "Ora":       { stars: 2 },
-    "Akelarre":  { stars: 3 }
-  }, x => x.stars === 2), "noma")
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
