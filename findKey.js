808b558ff4af4ee7e3f0ae0544a93a268a3e8370
Rosemary Ku <rosemary.ku@hotmail.com>
2022-01-13 13:29:02 -0500

    Add findKey function

---
+++ findKey.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function findKey(object, callback) {
+  let keyArray = Object.keys(object);
+  for (let key of keyArray) {
+    if (callback(object[key]) === true) {
+      return key;
+    }
+  }
+}
+
+// TESTS:
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)) // => "noma"
+
+
+assertEqual(findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2), "noma")
