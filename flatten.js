3326c76d59b0fc754c03dc7f83b66e09b362bc14
Rosemary Ku <rosemary.ku@hotmail.com>
2022-01-17 14:33:02 -0500

    Revise flatten.js

--- flatten.js
+++ flatten.js
@@ -1,36 +1,37 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length && arr1.every((val,index) => val === arr2[index])){
+const eqArrays = function (arr1, arr2) {
+  if (
+    arr1.length === arr2.length &&
+    arr1.every((val, index) => val === arr2[index])
+  ) {
     return true;
   } else {
     return false;
   }
 };
 
-
-const assertArraysEqual = function(arr1, arr2) {
+const assertArraysEqual = function (arr1, arr2) {
   let result = eqArrays(arr1, arr2);
   if (result) {
-    console.log(` ✅ Assertion Passed: ${arr1} === ${arr2} `)
+    console.log(` ✅ Assertion Passed: ${arr1} === ${arr2} `);
   } else {
-    console.log(` 🔴 Assertion Failed: ${arr1} !== ${arr2} `)
+    console.log(` 🔴 Assertion Failed: ${arr1} !== ${arr2} `);
   }
-}
+};
 
-const flatten = function(arr) {
-  let flatArr = []; 
-  for (let i = 0; i < arr.length; i++){
+const flatten = function (arr) {
+  let flatArr = [];
+  for (let i = 0; i < arr.length; i++) {
     // console.log(arr[i]);
-    if (Array.isArray(arr[i])){
-      for (let j = 0; j < arr[i].length ; j++){
-        flatArr.push(arr[i][j]); 
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        flatArr.push(arr[i][j]);
       }
     } else {
-      flatArr.push(arr[i]); 
+      flatArr.push(arr[i]);
     }
   }
   return flatArr;
-}
+};
 
-
-// TESTS: 
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // Should Pass 
+// TESTS:
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // Should Pass
